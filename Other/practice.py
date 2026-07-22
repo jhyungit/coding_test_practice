31491232f384649572bab77779a45b92ea825137
jhyungit <jh021199@gmail.com>
2026-07-22 23:34:36 +0900

    chore: update code

--- Other/practice.py
+++ Other/practice.py
@@ -1,35 +1,58 @@
+def manhattan(x,y):
+    return abs(x[0] - y[0]) + abs(x[1] - y[1])
+
 def solution(w, h, start, waypoints):
     INF = float("inf")
-    n = len(waypoints) # 경유지 개수
+    n = len(waypoints)
+    from_start, dist = [], []
 
-    def manhattan(a, b):
-        return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-    # 거리 미리 계산 (장애물 없으니 맨해튼) ← 이게 통째로 빠져 있었음
-    from_start = [manhattan(start, waypoints[i]) for i in range(n)]
-    dist = [[manhattan(waypoints[i], waypoints[j]) for j in range(n)] for i in range(n)]
+    for i in range(n):
+        from_start.append(manhattan(start, waypoints[i]))
+        temp = []
+        for j in range(n):
+            if i == j:
+                temp.append(0)
+                continue
+            temp.append(manhattan(waypoints[i], waypoints[j]))
+        dist.append(temp)
     
     # dp[mask][i] 경유지, 현재 위치
     dp = [[INF] * n for _ in range(1 << n)]
 
-       # 3. 기저 ← 이게 빠져서 inf 나왔음
+    # 3. 기저
     for i in range(n):
         dp[1 << i][i] = from_start[i]
+    
+    for mask in range(1 << n): # 어떤 상태 mask에서
+        for i in range(n): # 위치 i에서
+            # 방문 체크
+            if dp[mask][i] == INF or not (mask & (1 << i)): # i가 실제로 방문한 곳이어야함
+                continue
 
-    for mask in range(1<<n): # 어떤 상태 mask에서
-        for i in range(n): # 위치 i에서 
-            if dp[mask][i] == INF or not (mask & (1<<i)): # i가 방문 안 한 곳이면
-                continue 
             for j in range(n): # 아직 안 간 j로
-                if mask & (1<<j):
-                    continue
                 nmask = mask | (1 << j) # j를 추가한 새 집합
                 cost = dp[mask][i] + dist[i][j] # i -> j 가는 비용
                 if cost < dp[nmask][j]: # 더 작으면
-                    dp[nmask][j] = cost        
-        
+                    dp[nmask][j] = cost
+
     full = (1 << n) - 1
-    return min(dp[full][i] for i in range(n))
+    return min(dp[full])
+    
+    # for mask in range(1<<n): 
+    #     for i in range(n): # 위치 i에서 
+    #         if dp[mask][i] == INF or not (mask & (1<<i)): # i가 실제로 방문한 곳이어야함
+    #             continue 
+    #         for j in range(n): # 아직 안 간 j로
+    #             if mask & (1<<j):
+    #                 continue
+    #             nmask = mask | (1 << j) # j를 추가한 새 집합
+    #             cost = dp[mask][i] + dist[i][j] # i -> j 가는 비용
+    #             if cost < dp[nmask][j]: # 더 작으면
+    #                 dp[nmask][j] = cost        
+        
+    # full = (1 << n) - 1
+    # return min(dp[full][i] for i in range(n))
+    return 0
 
 print(solution(5, 5, (1,1), [(2,2), (1,4), (5,1)])) # 10
 
